3d7acf4633f8a4800db85ca279a2d2a27fbe74ab
Ihor Porotikov <sobikashi@gmail.com>
2019-03-09 13:23:31 +0200

    add fixes

---
+++ SimleSite/Controllers/PricingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimleSite.Controllers
+{
+    public class PricingController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Payment page";
+            return View();
+        }
+    }
+}
---
+++ SimleSite/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimleSite.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Title";
+            ViewBag.skypename = "newSkypeName";
+            return View();
+        }
+    }
+}
